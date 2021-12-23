94b2c2a48ee902455e093e0be32ab0ce6fd3de92
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-23 21:50:46 +0530

    programmes on if, if else if conditions

---
+++ Weekend.java
@@ -0,0 +1,8 @@
+class Weekend{
+      public static void main(String []args){
+        boolean weekend = true;
+          if(weekend){
+             System.out.println("sunday");
+          }
+      }
+}
---
+++ YearIf.java
@@ -0,0 +1,8 @@
+class YearIf{
+   public static void main(String []args){
+        boolean months = true;
+      if(months){
+          System.out.println("january");
+      }
+   }
+}
